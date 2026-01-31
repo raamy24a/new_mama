e3e3b6e4cd339297e33b97e1e24c002e3bfbb1ad
raamy24a <raamydib@gmail.com>
2026-01-31 02:02:08 +0100

    a

--- built-in/builtin_env.c
+++ built-in/builtin_env.c
@@ -6,7 +6,7 @@
 /*   By: radib <radib@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/18 14:36:38 by radib             #+#    #+#             */
-/*   Updated: 2026/01/29 18:19:55 by radib            ###   ########.fr       */
+/*   Updated: 2026/01/31 01:59:59 by radib            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,15 @@ int	call_env(t_env *env)
 	temp = env;
 	while (temp->next)
 	{
-		printf("%s=%s\n", temp->key, temp->value);
+		if (temp->status == 1)
+			printf("%s=%s\n", temp->key, temp->value);
+		else if (temp->status == 2)
+			printf("%s=\n", temp->key);
 		temp = temp->next;
 	}
-	printf("%s=%s\n", temp->key, temp->value);
+	if (temp->status == 1)
+			printf("%s=%s\n", temp->key, temp->value);
+	else if (temp->status == 2)
+			printf("%s=\n", temp->key);
 	return (0);
 }
